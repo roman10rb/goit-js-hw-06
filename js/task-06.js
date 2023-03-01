3a6665e821c466b3ab76774b188896bba0a17b2b
roman10rb <roma10rb@gmail.com>
2023-03-01 11:04:19 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,18 @@
 const inputEl = document.querySelector('#validation-input');
 const atribyte = inputEl.getAttribute('data-length');
-console.log(atribyte)
 const invalidEl = document.querySelector('#validation-input.invalid');
 
 const validation = (event) => {
     const lengthValue = event.target.value.length;
-    if (lengthValue === 0) {
+    if (lengthValue === Number(atribyte)) {
         event.target.classList.remove('invalid');
-        event.target.classList.remove('valid');
+        event.target.classList.add('valid');
     }
-    else if (lengthValue < atribyte || lengthValue > atribyte ) {
+    else if (lengthValue !== Number(atribyte) ) {
         event.target.classList.add('invalid');
         event.target.classList.remove('valid');
     } else {
-    event.target.classList.add('valid');
+    event.target.classList.remove('valid');
     event.target.classList.remove('invalid');}
     
 };
