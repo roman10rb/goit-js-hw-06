63db5eff925528bae9094259e9f9eb52181d0306
roman10rb <roma10rb@gmail.com>
2023-03-01 13:46:02 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,13 @@
-function getRandomHexColor() {
+ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
 const bodyEl = document.querySelector('body');
 const buttonEl = document.querySelector('.change-color');
 const colorEl = document.querySelector('.color');
 buttonEl.addEventListener('click', () => {
-  bodyEl.style.backgroundColor = getRandomHexColor();
-  colorEl.textContent = getRandomHexColor();
+  const colorRandom = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorRandom;
+  colorEl.textContent = colorRandom;
 });
